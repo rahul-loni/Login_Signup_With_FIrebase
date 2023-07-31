98b00192cce6bd4edcbfee72340bd70593a5390a
Rahul Kumar <rahulloni000@gmail.com>
2023-07-31 09:35:25 +0545

    Splash Screen comp

---
+++ app/src/main/java/com/example/login_signup_with_firebase/Splash_Screen.java
@@ -0,0 +1,44 @@
+package com.example.login_signup_with_firebase;
+
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash_Screen extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        init();
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.animation);
+        imageView.setAnimation(animation);
+        textView.setAnimation(animation);
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(9000);
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+    public void init(){
+        imageView=findViewById(R.id.img);
+        textView=findViewById(R.id.txt);
+    }
+}
